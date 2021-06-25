34ab34e8bad87979bb21418b8da19682c32a5ce8
Guzik98 <guzkac2@gmail.com>
2021-06-25 10:51:19 +0200

    Encoding password

--- src/main/java/com/example/blog/model/User.java
+++ src/main/java/com/example/blog/model/User.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
-
 
 @AllArgsConstructor
 @Data
--- src/main/java/com/example/blog/service/AuthService.java
+++ src/main/java/com/example/blog/service/AuthService.java
@@ -4,6 +4,7 @@ import com.example.blog.dto.RegisterRequest;
 import com.example.blog.model.User;
 import com.example.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,18 @@ public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
